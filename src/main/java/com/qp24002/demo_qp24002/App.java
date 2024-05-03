c299db403e3952c07a6a7a567f6c5c7c668e260e
Estefany Ponce <164693621+EstefanyPonce24002@users.noreply.github.com>
2024-05-03 15:17:32 -0600

    Add files via upload
    Initial commit

---
+++ src/main/java/com/qp24002/demo_qp24002/App.java
@@ -0,0 +1,18 @@
+package com.qp24002.demo_qp24002;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+			public String Hello ()  {
+				return "Hello World";
+			}
+	
+    public static void main( String[] args )
+    {
+		    	App hw = new App();
+		        System.out.println( hw.Hello ()  ) ;
+    }
+}
